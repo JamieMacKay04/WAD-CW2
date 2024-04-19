9af1901766a4906e7cb8fdf331be531a11fe4bb3
JMACKA206 <JMACKA206@caledonian.ac.uk>
2024-04-19 14:57:29 +0100

    Login time bby

--- app.js
+++ app.js
@@ -63,11 +63,6 @@ app.get('/profile', (req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-    res.status(404).render('lost', {
-        pageTitle: '404 Not Found'
-    });
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
